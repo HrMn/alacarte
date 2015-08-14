0ef2797074de4d64424d7226a38d2af1e11eeb00
HARI <harik87@gmail.com>
2015-08-14 23:23:46 +0530

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='OrderTaking',
+      version='1.0',
+      description='Order Taking Application',
+      author='Hari Menon',
+      author_email='',
+      url='http://www.python.org/sigs/distutils-sig/',
+     install_requires=['Flask>=0.7.2' , 'Flask-SQLAlchemy>=0.16'],
+     )
